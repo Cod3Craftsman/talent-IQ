406047b685d2d018edb945d6f907cc7f729f708c
Ankit Gupta <ag5756042@gmail.com>
2026-01-10 23:06:26 +0530

    dashboard page added

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -5,6 +5,10 @@ import { useCreateSession, useActiveSessions } from "../hooks/useSessions.js";
 import { useMyRecentSessions } from "../hooks/useSessions";
 import Navbar from "../components/Navbar.jsx";
 import WelcomeSection from "../components/WelcomeSection.jsx";
+import StatsCards from "../components/StatsCards.jsx";
+import ActiveSessions from "../components/ActiveSessions.jsx";
+import RecentSessions from "../components/RecentSessions.jsx";
+import CreateSessionModal from "../components/CreateSessionModal.jsx";
 
 function DashboardPage() {
   const navigate = useNavigate();
@@ -21,7 +25,7 @@ function DashboardPage() {
   const handleCreateRoom = () => {
     if (!roomConfig.problem || !roomConfig.difficulty) return;
     createSessionMutation.mutate({
-      problem: roomConfig.problem, difficulty: roomConfig.difficulty
+      problem: roomConfig.problem, difficulty: roomConfig.difficulty.toLowerCase()
     },
       {
         onSuccess: (data) => {
@@ -33,16 +37,48 @@ function DashboardPage() {
   }
 
   const activeSessions = activeSessionsData?.sessions || [];
-  const recentSessions = activeSessionsData?.sessions || [];
+  const recentSessions = recentSessionsData?.sessions || [];
 
+  const isUserInSession = (session) => {
+    if (!user.id) return false;
+    return session.host?.clerkId === user.id || session.participant?.clerkId === user.id
+  }
 
 
   return <>
     <div className="min-h-screen bg-base-300">
       <Navbar />
-      <WelcomeSection onCreateSession={() => setShowCreateModal(true)}/>
+      <WelcomeSection onCreateSession={() => setShowCreateModal(true)} />
+      {/* Grid Layout */}
+      <div className="container mx-auto px-6 pb-16">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+          <StatsCards
+            activeSessionsCount={activeSessions.length}
+            recentSessionsCount={recentSessions.length}
+
+          />
+          <ActiveSessions
+            sessions={activeSessions}
+            isLoading={loadingActiveSessions}
+            isUserInSession={isUserInSession}
+          />
+        </div>
+        <RecentSessions sessions={recentSessions}
+          isLoading={loadingRecentSessions}
+        />
+      </div>
     </div>
-  
+
+    <CreateSessionModal
+      isOpen={showCreateModal}
+      onClose={() => setShowCreateModal(false)}
+      roomConfig={roomConfig}
+      setRoomConfig={setRoomConfig}
+      onCreateRoom={handleCreateRoom}
+      isCreating={createSessionMutation.isPending}
+
+    />
+
   </>
 }
 
